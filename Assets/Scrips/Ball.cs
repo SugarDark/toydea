ed3a620cdbb85b7c09e8d60eb50cb152e43d58c6
pm83126492 <pm83126492@gmail.com>
2020-09-01 18:09:28 +0800

    0901 6:09

---
+++ Assets/Scrips/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Vector3 oldVel;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        oldVel = myRigidbody.velocity;
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        ContactPoint cp = c.contacts[0];
+        // calculate with addition of normal vector
+        // myRigidbody.velocity = oldVel + cp.normal*2.0f*oldVel.magnitude;
+
+        // calculate with Vector3.Reflect
+        myRigidbody.velocity = Vector3.Reflect(oldVel, cp.normal);
+
+        // bumper effect to speed up ball
+        myRigidbody.velocity += cp.normal * 2.0f;
+    }
+}
---
+++ Assets/Scrips/wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * 20f, ForceMode.Impulse);
+        }
+    }
+}
